e5d0259e912a547ce457cad4748e10f05278aaa4
Zoe Jastreb <zoejastreb9@gmail.com>
2026-02-02 20:11:44 +0100

    Poner el query donde va

--- BaseDeDatos/tiendaFancBD.sql
+++ BaseDeDatos/tiendaFancBD.sql
@@ -1,3 +1,7 @@
 drop database if exists tiendaFnac;
 create database tiendaFnac;
 use tiendaFnac;
+
+create table Users(
+user_id int not null unique
+);
--- tiendaFancBD.sql
+++
@@ -1,7 +0,0 @@
-drop database if exists tiendaFnac;
-create database tiendaFnac;
-use tiendaFnac;
-
-create table Users(
-user_id int not null unique
-);
