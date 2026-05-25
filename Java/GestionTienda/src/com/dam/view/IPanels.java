7b11590bc0918cd74f27a3a628218807f9459955
Damaga <damagasnake@gmail.com>
2026-05-25 08:58:19 +0200

    Interfaces

--- Java/GestionTienda/src/com/dam/view/IPanels.java
+++ Java/GestionTienda/src/com/dam/view/IPanels.java
@@ -1,5 +1,12 @@
 package com.dam.view;
 
+import com.dam.ctrl.Ctrl;
+
 public interface IPanels {
 
+    public void configurarVentana();
+
+    public void crearComponentes();
+
+    public void setControlador(Ctrl consultaResController);
 }
