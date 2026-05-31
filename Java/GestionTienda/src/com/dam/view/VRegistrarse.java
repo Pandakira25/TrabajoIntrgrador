2193aec734645b8138e59a2d3bc732dc3aff8190
Zoe Jastreb <zoejastreb9@gmail.com>
2026-05-31 12:09:59 +0200

    cambios de david

--- Java/GestionTienda/src/com/dam/view/VRegistrarse.java
+++ Java/GestionTienda/src/com/dam/view/VRegistrarse.java
@@ -1,7 +1,39 @@
 package com.dam.view;
 
+import com.dam.ctrl.Ctrl;
+import java.awt.Dimension;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class VRegistrarse extends JPanel implements IPanels {
 
+    private JLabel lblTitulo;
+    private Ctrl ctrl;
+
+    public VRegistrarse() {
+        absoluteLayout();
+        crearComponentes();
+        configurarVentana();
+    }
+
+    private void absoluteLayout() {
+        setLayout(null);
+    }
+
+    @Override
+    public void crearComponentes() {
+        lblTitulo = new JLabel("Registrarse");
+        lblTitulo.setBounds(110, 15, 180, 28);
+        add(lblTitulo);
+    }
+
+    @Override
+    public void configurarVentana() {
+        setPreferredSize(new Dimension(400, 250));
+    }
+
+    @Override
+    public void setControlador(Ctrl consultaResController) {
+        ctrl = consultaResController;
+    }
 }
